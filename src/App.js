bdd71db5743a91b4505c3b8d090588b6faece6b8
purikura09 <purikura09@gmail.com>
2021-10-10 01:25:03 +0200

    footer update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App 2021</div>;
+  return (
+    <div className="App">
+      Weather App 2021
+      <footer>This website is created by RBK</footer>
+    </div>
+  );
 }
 
 export default App;
