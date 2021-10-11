4e5bdec37da87f3c703b05b49624bf98943e6186
purikura09 <purikura09@gmail.com>
2021-10-11 23:37:12 +0200

    icon update but not appearing

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#ffffff"
+      color="#fff"
       size={60}
       animate={true}
     />
